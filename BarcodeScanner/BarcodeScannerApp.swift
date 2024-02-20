2dc601aac7d94dd601ebd66a12d5b4b9004b6606
Matt Watters <mwatt023@uottawa.ca>
2024-02-20 15:34:58 -0500

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Matt Watters on 2024-02-20.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
