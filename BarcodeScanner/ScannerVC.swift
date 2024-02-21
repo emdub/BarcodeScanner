0033bd2be0d28ed9377983e1ad5cfb6344b7dbda
Matt Watters <mwatt023@uottawa.ca>
2024-02-21 12:54:39 -0500

    initial commit

---
+++ BarcodeScanner/ScannerVC.swift
@@ -0,0 +1,8 @@
+//
+//  ScannerVC.swift
+//  BarcodeScanner
+//
+//  Created by Matt Watters on 2024-02-21.
+//
+
+import Foundation
---
+++ BarcodeScanner/ScannerView.swift
@@ -0,0 +1,18 @@
+//
+//  ScannerView.swift
+//  BarcodeScanner
+//
+//  Created by Matt Watters on 2024-02-21.
+//
+
+import SwiftUI
+
+struct ScannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScannerView()
+}
